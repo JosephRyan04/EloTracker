4f324b7b5e794c472de1da40d96f2194be6fcf2d
JosephRyan04 <lkyjosephryan@gmail.com>
2024-11-22 18:38:35 -0500

    Refactor imports for clean code

--- application/Models/User.py
+++ application/Models/User.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timezone
 from typing import List
 import sqlalchemy as sqla
-import sqlalchemy.orm as so
+from sqlalchemy.orm import mapped_column, Mapped
 from application import db
 
 class User(db.Model):
@@ -15,20 +15,20 @@ class User(db.Model):
             'gamecount': self.UpdateCount
         }
 
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    LastUpdate: so.Mapped[datetime] = so.mapped_column(
+    id: Mapped[int] = mapped_column(primary_key=True)
+    LastUpdate: Mapped[datetime] = mapped_column(
         index=True, default=lambda: datetime.now(timezone.utc), onupdate=datetime.utcnow)
-    DisplayName: so.Mapped[str] = so.mapped_column(sqla.String(16), unique=False)
-    ConnectCode: so.Mapped[str] = so.mapped_column(sqla.String(9), index=True, unique=True)
-    CurrentRank: so.Mapped[float] = so.mapped_column(sqla.Float)
-    UpdateCount: so.Mapped[int] = so.mapped_column(sqla.Integer, nullable=True)
-    MaxStreak: so.Mapped[int] = so.mapped_column(sqla.Integer, nullable=True)
-    CurrentStreak: so.Mapped[int] = so.mapped_column(sqla.Integer, nullable=True)
-    Continent: so.Mapped[str] = so.mapped_column(sqla.String(16), nullable=True)
-    GlobalRank: so.Mapped[int] = so.mapped_column(sqla.Integer, nullable=True)
-    PeakGlobal: so.Mapped[int] = so.mapped_column(sqla.Integer, nullable=True)
-    RegionalRank: so.Mapped[int] = so.mapped_column(sqla.Integer, nullable=True)
-    transactions: so.Mapped[List["Transaction"]] = db.relationship(back_populates="user")
+    DisplayName: Mapped[str] = mapped_column(sqla.String(16), unique=False)
+    ConnectCode: Mapped[str] = mapped_column(sqla.String(9), index=True, unique=True)
+    CurrentRank: Mapped[float] = mapped_column(sqla.Float)
+    UpdateCount: Mapped[int] = mapped_column(sqla.Integer, nullable=True)
+    MaxStreak: Mapped[int] = mapped_column(sqla.Integer, nullable=True)
+    CurrentStreak: Mapped[int] = mapped_column(sqla.Integer, nullable=True)
+    Continent: Mapped[str] = mapped_column(sqla.String(16), nullable=True)
+    GlobalRank: Mapped[int] = mapped_column(sqla.Integer, nullable=True)
+    PeakGlobal: Mapped[int] = mapped_column(sqla.Integer, nullable=True)
+    RegionalRank: Mapped[int] = mapped_column(sqla.Integer, nullable=True)
+    transactions: Mapped[List["Transaction"]] = db.relationship(back_populates="user")
 
     def __repr__(self):
         return '<User {}>'.format(self.ConnectCode)
